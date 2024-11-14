5b0a172b920ca4da86da76c21003987ca112192a
HanzhiXu33 <h5xu@ucsd.edu>
2024-11-14 08:18:27 +0800

    Update mockData.js

--- components/utils/mockData.js
+++ components/utils/mockData.js
@@ -1,6 +1,6 @@
-const mockData = [
-  { date: "2024-11-15", title: "CS Class" },
-  { date: "2024-11-18", title: "Assignment Deadline" }
+const mockEvents = [
+  { id: "1", title: "CS Class", date: "2024-11-15", time: "10:00" },
+  { id: "2", title: "Assignment Due", date: "2024-11-18", time: "23:59" },
 ];
 
-export default mockData;
+export default mockEvents;
